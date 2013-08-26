6f4d214ed6d510651ff0389a2c6204ff15ff562c
Shea Dawson <shea@livesource.co.nz>
2013-08-26 15:06:56 +1000

    MINOR updated config to a) not force module dir name, b) use yml to apply extensions

--- _config.php
+++ _config.php
@@ -1,12 +1,3 @@
 <?php
 
-define('QUICKADDNEW_MODULE', basename(dirname(__FILE__)));
-
-if(QUICKADDNEW_MODULE != 'quickaddnew') {
-	throw new Exception(
-		"The addnew module must be in a directory named 'addnew', not " . QUICKADDNEW_MODULE
-	);
-}
-
-Object::add_extension('DropdownField', 'QuickAddNewExtension');
-Object::add_extension('ListboxField', 'QuickAddNewExtension');
+if(!defined('QUICKADDNEW_MODULE')) define('QUICKADDNEW_MODULE', rtrim(basename(dirname(__FILE__))));
