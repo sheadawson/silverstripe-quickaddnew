9484b8e395c347424b3c645968450c01835eb197
Milan Jelicanin <milan.jelicanin@fractas.com>
2016-03-18 10:17:06 +0100

    added croatian translation

---
+++ javascript/lang/hr.js
@@ -0,0 +1,7 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+    console.error('Class ss.i18n not defined');
+} else {
+    ss.i18n.addDictionary('hr', {
+        'QUICKADDNEW.AddNew' : "Dodaj novi"
+    });
+}
