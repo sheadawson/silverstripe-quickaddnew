13d034c000dc43ac3d825968c1100f57250131d0
Hendrik Schaper <hendrik@schaper-software.de>
2018-09-20 13:17:29 +0200

    PHP 7.2 compatibility
    SS_Object instead of Object

--- _config.php
+++ _config.php
@@ -1,3 +1,9 @@
 <?php
 
 if(!defined('QUICKADDNEW_MODULE')) define('QUICKADDNEW_MODULE', rtrim(basename(dirname(__FILE__))));
+
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) {
+    class_alias('Object', 'SS_Object');
+}
