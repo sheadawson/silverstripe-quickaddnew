7f0e7870639da60734e261117866e21be9a2c05f
Thomas <thomas@lekoala.be>
2024-11-08 10:07:27 +0100

    french locale

---
+++ client/javascript/lang/fr.js
@@ -0,0 +1,7 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+    console.error('Class ss.i18n not defined');
+} else {
+    ss.i18n.addDictionary('fr', {
+        'QUICKADDNEW.AddNew' : "Créer"
+    });
+}
